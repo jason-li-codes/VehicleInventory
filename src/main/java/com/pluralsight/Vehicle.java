c973108625aad44aed577258ed8d61b17c1d10ba
lijas <lijason991@gmail.com>
2025-10-03 15:32:41 -0400

    finished Vehicle class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -17,5 +17,53 @@ public class Vehicle {
 
     }
 
+    public Vehicle(long vehicleID, String makeModel, String color, int odometerReading, float price) {
+        this.vehicleID = vehicleID;
+        this.makeModel = makeModel;
+        this.color = color;
+        this.odometerReading = odometerReading;
+        this.price = price;
+    }
+
+    public long getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(long vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+
+    public void setOdometerReading(int odometerReading) {
+        this.odometerReading = odometerReading;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
 
 }
