14221ac0411f602e732219da77b049eef172a25b
lijas <lijason991@gmail.com>
2025-10-03 15:29:35 -0400

    first commit

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Vehicle {
+
+    private long vehicleID;
+    private String makeModel;
+    private String color;
+    private int odometerReading;
+    private float price;
+
+    public Vehicle() {
+        this.vehicleID = 0;
+        this.makeModel = "";
+        this.color = "";
+        this.odometerReading = 0;
+        this.price = 0;
+
+    }
+
+
+}
