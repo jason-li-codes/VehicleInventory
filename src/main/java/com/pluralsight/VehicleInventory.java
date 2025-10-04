97a761ebcda459741f3f722188a5e1bd201888cc
lijas <lijason991@gmail.com>
2025-10-03 21:21:16 -0400

    finished searchByMakemodel

--- src/main/java/com/pluralsight/VehicleInventory.java
+++ src/main/java/com/pluralsight/VehicleInventory.java
@@ -4,17 +4,19 @@ import java.util.*;
 
 public class VehicleInventory {
 
+    public static int currentInventory = 0;
+
     public static void main(String[] args) {
 
         Vehicle[] carshop = new Vehicle[20];
-        carshop[0] = new Vehicle(101121, "Ford Explorer", "red", 45000, 13500);
-        carshop[1] = new Vehicle(101122, "Toyota Camry", "blue", 60000, 11000);
-        carshop[2] = new Vehicle(101123, "Chevrolet Malibu", "black", 50000, 9700);
-        carshop[3] = new Vehicle(101124, "Honda Civic", "white", 70000, 7500);
-        carshop[4] = new Vehicle(101125, "Subaru Outback", "green", 55000, 14500);
-        carshop[5] = new Vehicle(101126, "Jeep Wrangler", "yellow", 30000, 16000);
+        carshop[0] = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
+        carshop[1] = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000);
+        carshop[2] = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700);
+        carshop[3] = new Vehicle(101124, "Honda Civic", "White", 70000, 7500);
+        carshop[4] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
+        carshop[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000);
 
-        int currentInventory = carCounter(carshop);
+        currentInventory += 6;
 
         Scanner input = new Scanner(System.in);
 
@@ -57,33 +59,21 @@ public class VehicleInventory {
 
     }
 
-    public static int carCounter(Vehicle[] carshop) {
+    public static void printVehicleInfo(Vehicle car) {
 
-        int count = 0;
-
-        for (Vehicle car : carshop) {
-            if (car != null) {
-                count += 1;
-            }
-        }
-
-        return count;
+        System.out.printf("%s %s - ID: %d, odometer reading: %d, price: $%.2f",
+                car.getColor(), car.getMakeModel(), car.getVehicleID(),
+                car.getOdometerReading(), car.getPrice());
 
     }
 
     public static void listAllVehicles(Vehicle[] carshop) {
 
-        for (int i = 0; i < carshop.length; i++) {
+        for (int i = 0; i < currentInventory; i++) {
             Vehicle car = carshop[i];
-            if (car != null) {
-                System.out.printf("""
-                    Vehicle #%d. %s %s
-                    ID: %d, odometer reading: %d, price: %.2f
-                    """, i, car.getColor(), car.getMakeModel(), car.getVehicleID(),
-                        car.getOdometerReading(), car.getPrice());
-            }
+            System.out.printf("Vehicle #%d", i);
+            printVehicleInfo(car);
         }
-
     }
 
     public static void searchByMakemodel(Vehicle[] carshop, Scanner input) {
@@ -91,15 +81,12 @@ public class VehicleInventory {
         System.out.println("What make/model are you looking for?");
         String searchMakemodel = input.nextLine();
 
+        System.out.println("Here are cars matching that make/model:");
         for (Vehicle car : carshop) {
-            if (car.getMakeModel().contains(searchMakemodel)) {
-
+            if (car.getMakeModel().toLowerCase().contains(searchMakemodel.toLowerCase())) {
+                printVehicleInfo(car);
             }
         }
-
-
-
-
 
     }
 
